5bf0869613a24957ea4e2fef0ec7074e9845a717
Macaulay-Reiss Christian <christianm@cardiff.ac.uk>
2018-12-27 11:58:44 +0000

    created the session and user, the signup has been created but currently isnt sending data

---
+++ server/routes/api/signin.js
@@ -0,0 +1,82 @@
+const User = require('../../models/User')
+
+module.exports = (app) => {
+    /*
+     * Sign up
+     */
+    app.post('/api/account/signup', (req, res, next) => {
+        const { body } = req;
+        const {
+            firstName,
+            lastName,
+            password
+        } = body;
+
+        let {
+            email
+        } = body;
+
+        if(!firstName) {
+            return res.send({
+                success: false,
+                message: 'Error: Missing First name'
+            });
+        }
+        if(!lastName) {
+            return res.send({
+                success: false,
+                message: 'Error: Missing Last name'
+            });
+        }
+        if(!email) {
+            return res.send({
+                success: false,
+                message: 'Error: Missing email'
+            });
+        }
+        if(!password) {
+            return res.send({
+                success: false,
+                message: 'Error: Missing password'
+            });
+        }
+
+        email = email.toLowerCase();
+
+        User.find({
+            email: email
+        }, (err, previousUsers) => {
+            if(err) {
+                return res.send({
+                    success: false,
+                    message: 'Error: Server Error'
+                });
+            } else if (previousUsers.length > 0) {
+                return res.send({
+                    success: false,
+                    message: 'Error: Account already exists'
+                });
+            }
+        })
+        /*
+         * Save the new User
+         */
+        const newUser = new User();
+
+        newUser.email = email;
+        newUser.firstName = firstName;
+        newUser.lastName = lastName;
+        newUser.password = newUser.generateHash(password);
+        newUser.save((err, user) => {
+            if(err) {
+                return res.end({                    success: false,
+                    message: 'Error: Server Error'
+                });
+            }
+            return res.end({
+                success: true,
+                message: 'Account Created'
+            });
+        });
+    });
+}
