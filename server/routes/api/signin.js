603904717f9f7e7c890dd93b7a61c61ef5a9ae11
Macaulay-Reiss Christian <christianm@cardiff.ac.uk>
2019-01-02 14:31:27 +0000

    Verify user by token

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -151,4 +151,35 @@ module.exports = (app) => {
 
         });
     });
+    /*
+     * Verify user
+     */
+    app.get('/api/account/verify', (req, res, next) => {
+        const { query } = req;
+        const { token } = query;
+
+        UserSession.find({
+            _id: token,
+            isDeleted: false
+        }, (err, sessions) =>{
+            if(err) {
+                return res.send({
+                    success: false,
+                    message: "Error: Server error"
+                });
+            }
+
+            if(sessions.length != 1) {
+                return res.send({
+                    success: false,
+                    message: "Error: Invalid"
+                });
+            } else {
+                return res.send({
+                    success: true,
+                    message: "Verified"
+                });
+            }
+        });
+    });
 }
