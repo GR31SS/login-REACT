15b3622b920b2696baa0bd19f9951867821f42a4
Macaulay-Reiss Christian <christianm@cardiff.ac.uk>
2019-01-02 17:35:05 +0000

    Created the logout function

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -182,4 +182,30 @@ module.exports = (app) => {
             }
         });
     });
+    /*
+     * Logout
+     */
+    app.get('/api/account/logout', (req, res, next) => {
+        const { query } = req;
+        const { token } = query;
+
+        UserSession.findOneAndUpdate({
+            _id: token,
+            isDeleted: false
+        }, {
+            $set: { isDeleted: true }
+        }, null, (err, sessions) => {
+            if(err) {
+                return res.send({
+                    success: false,
+                    message: "Error: Server error"
+                });
+            }
+            return res.send({
+                success: true,
+                message: "Good"
+            });
+
+        });
+    });
 }
